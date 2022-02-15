09f9f2e66ce7b9d37b56e15afe72e8b728e24de3
이진실 <ljsil1204@naver.com>
2022-02-15 16:00:11 +0900

    추가 로직 답안  - else if로 보완 로직 작성
    EditText id 값 변경

--- app/src/main/java/com/neppplus/kotlingrammaruitest_20220215/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlingrammaruitest_20220215/MainActivity.kt
@@ -12,15 +12,22 @@ class MainActivity : AppCompatActivity() {
 
         btnLogin.setOnClickListener {
 
-            var inputId = inputIdTxt.text.toString()
-            var inputPassword = inputPassword.text.toString()
+            var inputId = edtID.text.toString()
+            var inputPassword = edtPW.text.toString()
 
             if (inputId == "admin" && inputPassword == "asdf") {
+//                로그인 성공
                 Toast.makeText(this, "관리자님 환영합니다", Toast.LENGTH_SHORT).show()
             }
-            else {
-                Toast.makeText(this, "로그인에 실패했습니다", Toast.LENGTH_SHORT).show()
+            else if (inputId != "admin") {
+//                아이디 실패
+                Toast.makeText(this, "아이디가 잘못되었습니다", Toast.LENGTH_SHORT).show()
             }
+            else {
+//                로그인에 실패했고 / 아이디는 맞게 넣었다 => 비번만 틀렸다고 간주하자.
+                Toast.makeText(this, "비밀번호가 잘못되었습니다", Toast.LENGTH_SHORT).show()
+            }
+
 
         }
 
